cb241d5644c8dee3e49c74b788ae8341df39e8aa
DimaKrupenko <dimakrupenko77@gmail.com>
2023-03-17 13:49:45 +0200

    upd: make owner

--- controllers/contacts/add.js
+++ controllers/contacts/add.js
@@ -9,7 +9,8 @@ const add = async (req, res, next) => {
       error.status = 400;
       throw error;
     }
-    const newContact = await Contacts.create(req.body);
+    const { _id } = req.user;
+    const newContact = await Contacts.create({ ...req.body, owner: _id });
     res.status(201).json({
       status: 'success',
       code: 201,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+const { auth } = require('../../middlewares/index');
+
 const { contacts: ctrl } = require('../../controllers/index');
 
 router.get('/', ctrl.getAll);
 
 router.get('/:contactId', ctrl.getById);
 
-router.post('/', ctrl.add);
+router.post('/', auth, ctrl.add);
 
 router.delete('/:contactId', ctrl.remove);
 
